5f0db8b58dff2556eb3a75acd2318d5a44de7789
Natalie Nguyen <Natalie.2.nguyen@ucdenver.edu>
2024-10-13 12:37:56 -0600

    chose random forest

--- server/src/model.py
+++ server/src/model.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 from nltk.corpus import stopwords
+from sklearn.ensemble import RandomForestClassifier
+
 
 # approach:
 # clean and preprocess the questions (remove punctuation, lowercase the text, etc.).
@@ -32,62 +30,15 @@ vectorizer = TfidfVectorizer()
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
-
-# Train a Logistic Regression model
-model = LogisticRegression()
-model.fit(X_train_tfidf, y_train)
-
-# Predict the intent on the test set
-y_pred = model.predict(X_test_tfidf)
-
-# Evaluate the model
-print(f'Logistic Regression Accuracy: {accuracy_score(y_test, y_pred)}')
-# print(classification_report(y_test, y_pred))
-print()
-##############################################
-from sklearn.linear_model import SGDClassifier
-
-sgd = Pipeline([('vect', CountVectorizer()),
-                ('tfidf', TfidfTransformer()),
-                ('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42, max_iter=5, tol=None)),
-               ])
-sgd.fit(X_train, y_train)
-
-y_pred = sgd.predict(X_test)
-
-print('Linear Support Vector machine accuracy %s' % accuracy_score(y_pred, y_test))
-# print(classification_report(y_test, y_pred))
-print()
-
-###################
-from sklearn.ensemble import RandomForestClassifier
-
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(X_train_tfidf, y_train)
 y_pred_rf = rf.predict(X_test_tfidf)
 
+print(pd.DataFrame({'Predicted': y_pred_rf, 'Actual': y_test.values}))
 print(f'Random Forest Accuracy: {accuracy_score(y_test, y_pred_rf)}')
 print()
-# # print(classification_report(y_test, y_pred))
-# print()
 
-from sklearn.ensemble import GradientBoostingClassifier
-
-gb = GradientBoostingClassifier(random_state=42)
-gb.fit(X_train_tfidf, y_train)
-y_pred_gb = gb.predict(X_test_tfidf)
-
-print(f'Gradient Boosting Accuracy: {accuracy_score(y_test, y_pred_gb)}')
-print()
-
-from sklearn.model_selection import GridSearchCV
-
-param_grid = {'C': [0.1, 1, 10, 100, 1000], 'penalty': ['l2']}
-grid = GridSearchCV(LogisticRegression(), param_grid, cv=5)
-grid.fit(X_train_tfidf, y_train)
-
-print(f'Best parameters: {grid.best_params_}')
-print(f'Best accuracy: {grid.best_score_}')
+# You can also print them side by side for better comparison
 
 
 
