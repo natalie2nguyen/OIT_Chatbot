1f6ed6aca2a9119b5721b79fc3668a94ab31730b
Natalie Nguyen <Natalie.2.nguyen@ucdenver.edu>
2024-10-30 13:39:18 -0600

    fixed tests

--- server/tests/test_chatbot.py
+++ server/tests/test_chatbot.py
@@ -20,7 +20,7 @@ if parent_dir not in sys.path:
 # Import the preprocess_input function from src.chatbot
 from src.chatbot import preprocess_input
 
-from src.chatbot import match_intent_to_response
+from src.chatbot import match_intent_to_response, read_data
 
 def test_preprocess_normal_input():
     input_text = "This is a sample message with punctuation! Very nice."
@@ -47,19 +47,28 @@ def test_preprocess_lemmatization():
     expected_output = "running runner ran"
     assert preprocess_input(input_text) == expected_output
 
-def test_match_intent_to_response(): 
+def test_match_intent_to_response():
+    intent = "Password_reset"
+    expected_output = "Please click on the following link to reset your password: https://myaccount.ucdenver.edu/change-password"
+    dictionary = {'Forgot_credentials':"Please specify which credential you need: Password, Username, or Email",
+                      "Password_reset":"Please click on the following link to reset your password: https://myaccount.ucdenver.edu/change-password", 
+                      "Username": "To obtain your username, you can go to the following website and click on 'Forgot my username?' under the 'Next Step' button. Your username will be sent to the email you registered your account with: https://myaccount.ucdenver.edu/change-password"
+                      } 
+    assert match_intent_to_response(intent, dictionary) == expected_output
+
+def test_read_data():
     with patch('model.pd.read_excel') as mock_read_excel:
-        mocked_data = pd.DataFrame({
-            "intent": ["Forgot_credentials", "Password_reset", "Username"], 
-            "response": ["Please specify which credential you need: Password, Username, or Email",
-                         "Please click on the following link to reset your password: https://myaccount.ucdenver.edu/change-password", 
-                         "To obtain your username, you can go to the following website and click on 'Forgot my username?' under the 'Next Step' button. Your username will be sent to the email you registered your account with: https://myaccount.ucdenver.edu/change-password"]
-                         
-        })
+        mocked_data = pd.DataFrame([
+            ["Forgot_credentials", "Please specify which credential you need: Password, Username, or Email"],
+            ["Password_reset", "Please click on the following link to reset your password: https://myaccount.ucdenver.edu/change-password"],
+            ["Username", "To obtain your username, you can go to the following website and click on 'Forgot my username?' under the 'Next Step' button. Your username will be sent to the email you registered your account with: https://myaccount.ucdenver.edu/change-password"]
+        ])
         mock_read_excel.return_value = mocked_data
-        
-        intent = "Password_reset"
-        expected_output = "Please click on the following link to reset your password: https://myaccount.ucdenver.edu/change-password"
+        dictionary = {
+            'Forgot_credentials': "Please specify which credential you need: Password, Username, or Email",
+            "Password_reset": "Please click on the following link to reset your password: https://myaccount.ucdenver.edu/change-password",
+            "Username": "To obtain your username, you can go to the following website and click on 'Forgot my username?' under the 'Next Step' button. Your username will be sent to the email you registered your account with: https://myaccount.ucdenver.edu/change-password"
+        }
 
-        assert match_intent_to_response(intent) == expected_output
-
+        result = read_data()
+        assert result == dictionary
